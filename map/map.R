e9aa4d4be5b20b55161ca771a7db06aec3d71bbf
Akshay S Dinesh <akshay@learnlearn.in>
2025-10-14 00:14:59 +0530

    feat: automatically create docx too

--- map/map.R
+++ map/map.R
@@ -7,6 +7,8 @@ library(cowplot)
 library(terra)
 library(ggrepel)
 library(here)
+library(officer)
+library(flextable)
 
 here::i_am("map/map.R")
 source(here("map/functions.R"))
@@ -210,6 +212,8 @@ CASSAVA <- "(19)cassava_and_products"
 MAIZE <- "(16)maize_and_products"
 RICE <- "(15)rice_and_products"
 WHEAT <- "(14)wheat_and_products"
+SDI <- "(4)sdi_2021"
+ANIMAL <- "animal_protein_gm/cap/d"
 
 
 # Africa Maps
@@ -345,3 +349,208 @@ ggsave(
 )
 
 print("Maps created successfully!")
+
+# Now create report
+
+create_top_10_table <- function(crop_column, crop_name, table_num, data = merged_df) {
+  cols_to_select <- c(
+    "name_long", crop_column, SDI, ANIMAL, STUNTING, WASTING
+  )
+
+  table_data <- data %>%
+    as_tibble() %>%
+    filter(!is.na(!!sym(crop_column))) %>%
+    select(all_of(cols_to_select)) %>%
+    arrange(desc(!!sym(crop_column))) %>%
+    slice(1:10) %>%
+    mutate(S.No = row_number()) %>%
+    select(
+      S.No,
+      Country = name_long,
+      Consumption = !!sym(crop_column),
+      SDI = !!sym(SDI),
+      Animal_Protein = !!sym(ANIMAL),
+      Stunting = !!sym(STUNTING),
+      Wasting = !!sym(WASTING)
+    )
+
+  col_names <- c(
+    "S.No",
+    "Country",
+    paste0("Consumption ", crop_name, " (gm/capita/day)"),
+    "SDI",
+    "Animal Protein (gm/capita/day)",
+    "Stunting Prevalence (%)",
+    "Wasting Prevalence (%)"
+  )
+
+  names(table_data) <- col_names
+
+  ft <- flextable(table_data) %>%
+    theme_vanilla() %>%
+    set_caption(
+      caption = paste0("Table ", table_num, ": Top 10 LMIC Countries Consuming ", crop_name, " Protein")
+    ) %>%
+    autofit() %>%
+    width(width = 4)
+
+  return(ft)
+}
+
+
+create_crop_section <- function(doc, crop_name, crop_variable, writeup_para, merged_data, africa_map_file, world_map_file, table_num, fig_num) {
+  doc <- doc %>%
+    body_add_par(crop_name, style = "heading 3")
+
+
+  ft <- create_top_10_table(crop_variable, crop_name, table_num, merged_data)
+  doc <- doc %>%
+    body_add_flextable(ft) %>%
+    body_add_par("", style = "Normal")
+
+  doc <- doc %>%
+    body_add_par(writeup_para, style = "Normal") %>%
+    body_add_par("", style = "Normal")
+
+  doc <- doc %>%
+    body_add_img(src = africa_map_file, width = 6, height = 4) %>%
+    body_add_caption(block_caption(
+      label = paste("S Figure ", fig_num, ": Political map of Africa showing LMIC countries having high ", crop_name, " consumption"),
+      style = "Normal"
+    )) %>%
+    body_add_par("", style = "Normal")
+
+
+  doc <- doc %>%
+    body_add_img(src = world_map_file, width = 6, height = 4) %>%
+    body_add_caption(block_caption(
+      label = paste("Figure ", fig_num + 1, ": World map showing LMIC countries having high ", crop_name, " consumption"),
+      style = "Normal"
+    )) %>%
+    body_add_par("", style = "Normal")
+
+  return(doc)
+}
+
+create_world_section <- function(doc, caption, world_map_file) {
+  doc <- doc %>%
+    body_add_img(src = world_map_file, width = 6, height = 4) %>%
+    body_add_caption(block_caption(
+      label = caption,
+      style = "Normal"
+    )) %>%
+    body_add_par("", style = "Normal")
+
+  return(doc)
+}
+
+
+create_word_report_manual <- function(data, output_file = "Report_Crop_Analysis_Manual.docx") {
+  doc <- read_docx()
+
+  doc <- doc %>%
+    body_add_par("(J) Maps and tables of top staple consuming countries:", style = "heading 2") %>%
+    body_add_par(
+      "The tables below (S Table 39 to S Table 44) show the top 10 countries consuming the starchy staples discussed. Their SDI, animal protein consumption as well as stunting and wasting prevalence are shown along with for clarity. Any of the numbers of top 10 countries, if in Africa are shown along with in the map of Africa, indicating the geographic patterns of contiguity in crop cultivation. The thresholds low-to- medium-to- high for child stunting and wasting prevalence in top 10 countries are as per S Table 4 (from the Joint malnutrition estimates 2023). (11) Of note, also is the mirroring of distribution of high prevalence of wasting in Fig 1 in the manuscript and that of millet as well as sorghum consumption in Africa(S Fig 44 & S Fig 46) in the Sahel.",
+      style = "Normal"
+    ) %>%
+    body_add_break()
+
+
+  doc <- create_crop_section(
+    doc = doc,
+    crop_name = "Sorghum",
+    crop_variable = SORGHUM,
+    writeup_para = "The top 10 countries consuming Sorghum are all located in the arid Sahel region of Africa. The highest is Sudan in North Africa, but most others are from Western Sub-Saharan Africa with the exception of South Sudan and Ethiopia which are from Eastern Sub-Saharan Africa. They have among them highest wasting prevalence of 22.7 in South Sudan and the lowest 4.3 in Cameroon. Stunting ranges from a high of 47.4 for Niger to 21.8 for Burkina Faso, both falling above the very high and high thresholds as per JME estimates thresholds.",
+    merged_data = data,
+    africa_map_file = "africa_sorghum.png",
+    world_map_file = "sorghum.png",
+    table_num = 41,
+    fig_num = 44
+  )
+
+  doc <- create_crop_section(
+    doc = doc,
+    crop_name = "Millet",
+    crop_variable = MILLET,
+    writeup_para = "Among the top 10 countries consuming millets, the highest was Mali with 13.64 gm per capita per day. The highest stunting prevalence was with 47.4 for Niger and the lowest was 13.6 for Gambia. The highest wasting prevalence was 16.3 for Sudan and lowest 5.1 for Gambia. Most countries are from Sahel region of Africa. The stunting and wasting prevalence ranges widely as per JME thresholds from medium to high and very high.",
+    merged_data = data,
+    africa_map_file = "africa_millet.png",
+    world_map_file = "millet.png",
+    table_num = 42,
+    fig_num = 46
+  )
+
+  doc <- create_crop_section(
+    doc = doc,
+    crop_name = "Cassava",
+    crop_variable = CASSAVA,
+    writeup_para = "The location of top countries consuming cassava is South of the Sahel inAfrica. Four of top 10 Cassava consuming countries are from CentralSub-Saharan Africa. All other 6 among top 10 countries are from126126adjoining Eastern and Western Sub-Saharan Africa. The highestconsumption of cassava protein was 11.1 of DRC, which wasconsiderably lower than top consumers of other staples because of thevery low protein content of cassava. The highest stunting prevalencewas seen in Burundi with 56.5%(which was the highest among allcountries as well). Of note, is that among these countries the lowestanimal protein consumption was Burundi with 3.1 gm per capita per day.",
+    merged_data = data,
+    africa_map_file = "africa_cassava.png",
+    world_map_file = "cassava.png",
+    table_num = 43,
+    fig_num = 48
+  )
+
+  doc <- create_crop_section(
+    doc = doc,
+    crop_name = "Maize",
+    crop_variable = MAIZE,
+    writeup_para = "Five of the top 10 maize consuming countries were located in Southern Sub-Saharan Africa while the remainder 3 were from Central Latin America and 1 from Tropical Latin America. The highest stunting prevalence among them was Guatemala with a 43.5% prevalence and lowest was Paraguay at 3.4%.Wasting was consistently low ranging from  129  129  Zambia with 4.2 which was the highest to Guatemala with 0.8, the lowest.",
+    merged_data = data,
+    africa_map_file = "africa_maize.png",
+    world_map_file = "maize.png",
+    table_num = 44,
+    fig_num = 50
+  )
+
+  doc <- create_crop_section(
+    doc = doc,
+    crop_name = "Rice",
+    crop_variable = RICE,
+    writeup_para = "All the top 10 rice consuming countries were from South Asia and South East Asia, with the exception of Madagascar which was from Eastern Sub-Saharan Africa. Highest stunting prevalence among them was Madagascar with 38.6% and lowest was Thailand with 11.8. Wasting prevalence ranged from a high of 15.1 for Sri Lanka and a low of 4.7 for Vietnam.",
+    merged_data = data,
+    africa_map_file = "africa_rice.png",
+    world_map_file = "rice.png",
+    table_num = 45,
+    fig_num = 52
+  )
+
+  doc <- create_crop_section(
+    doc = doc,
+    crop_name = "Wheat",
+    crop_variable = WHEAT,
+    writeup_para = "All the top wheat consuming countries were from North Africa and Middle East as well as Central Asia with the exception of Serbia which was from Central Europe. They all had low to very low wasting prevalence and low to medium stunting prevalence.",
+    merged_data = data,
+    africa_map_file = "africa_wheat.png",
+    world_map_file = "wheat.png",
+    table_num = 46,
+    fig_num = 54
+  )
+
+  doc %>%
+    body_add_par("World", style = "heading 3")
+
+  doc <- create_world_section(
+    doc = doc,
+    caption = "S Fig 56: World map of stunting prevalence among LMICs",
+    world_map_file = "stunting.png"
+  )
+
+  doc <- create_world_section(
+    doc = doc,
+    caption = "S Fig 57: World map of wasting prevalence among LMICs",
+    world_map_file = "wasting.png"
+  )
+
+
+  print(paste("Saving Word document to:", here(output_file)))
+  print(doc, target = here(output_file))
+  print("Word document created successfully!")
+}
+
+create_word_report_manual(
+  data = merged_df,
+  output_file = "Section J - Supplementary.docx"
+)
